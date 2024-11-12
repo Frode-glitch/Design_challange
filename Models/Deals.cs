ceb203dc8f68f4148e97386fb9f8517ad8a28cb8
548220 <548220@student.fontys.nl>
2024-11-12 10:35:54 +0100

    bijna af

---
+++ Models/Deals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_challenge.Models
+{
+    public class Deals
+    {
+
+        public int Id { get; set; }
+        public string Header { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,15 +1,49 @@
-﻿namespace Design_challenge
+﻿using Design_challenge.Models;
+
+namespace Design_challenge
 {
     public partial class MainPage : ContentPage
     {
+
+        public List<Deals> UniqueDeals {  get; set; }
+
+
         int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+            LoadData();
+            BindingContext = this;
         }
 
-        
+        private void LoadData()
+        {
+            UniqueDeals = new List<Deals>
+    {
+        new Deals
+        {
+            Id = 1,
+            Header = "deal1.png",
+            Name = " ",
+            Description = "Parkeerplaatsen vrij: 120"
+        },
+        new Deals
+        {
+            Id = 2,
+            Header = "deal2.png",
+            Name = "Korting op TV's van LG",
+            Description = "Komend weekend korting tot wel 40% op LG TV's"
+        },
+        new Deals
+        {
+            Id = 3,
+            Header = "deal3.png",
+            Name = "Gehaktballen Donderdag",
+            Description = "Deze maand elke donderdag deal gehaktballen"
+        }
+    };
+        }
 
 
         private async void Button_Clicked(object sender, EventArgs e)
